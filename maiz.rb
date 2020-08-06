ab856a81557bf9f14db4b44cb3b52e89c18fe2ba
Toshiaki Ishikura <craccho@gmail.com>
2020-08-07 05:57:43 +0900

    accidentally overridden the require method

--- maiz.rb
+++ maiz.rb
@@ -15,17 +15,16 @@ class Maiz < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  set(:require) do |key|
+  set(:requires) do |key|
     condition do
       case key
       when :access_token
         access_token = @my_env[:session][:access_token]
-        unless access_token
+        if access_token
+          @my_env[:access_token] = access_token
+        else
           @my_env[:session][:return_to] = request.path_info
           redirect '/oauth/request_user_auth'
-          halt
-        else
-          @my_env[:access_token] = access_token
         end
       end
     end
@@ -49,7 +48,7 @@ class Maiz < Sinatra::Base
     redirect '/'
   end
 
-  get '/', require: :access_token do
+  get '/', requires: :access_token do
     uri = 'https://api.zaim.net/v2/home/user/verify'
     result = Zaim::OauthConsumer::Operation::CallApi.call(uri: uri, method: :get, **@my_env)
     if result.success?
